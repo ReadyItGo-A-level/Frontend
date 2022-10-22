e2c4f1d384d3ff2c4380d48aa059b3544adbcaca
gksgpd97 <gksgpd97@naver.com>
2022-10-22 18:51:03 +0900

    feat:> 술전체, 술검색 1차 완료(이전 커밋 add 덜 돼서 재커밋) #48

---
+++ app/src/main/java/com/example/a_level/allalcohol/model/response/Alcohol.kt
@@ -0,0 +1,21 @@
+package com.example.a_level.allalcohol.model.response
+
+data class Alcohol(
+    val id: Long?,
+    val name: String?,
+    val type: String?,
+    val category: String?,
+    val volume: String?,
+    val size: String?,
+    val price: Int?,
+    val nation: String?,
+    val hit: Long?,
+    val image: String?,
+    val flavor: String?,
+    val food: String?,
+    val info: String?,
+    val body: Int?,
+    val sugar: Int?,
+    val tannins: Int?,
+    val acidity: Int?
+)
---
+++ app/src/main/java/com/example/a_level/allalcohol/model/response/AllAlcoholResponse.kt
@@ -0,0 +1,12 @@
+package com.example.a_level.allalcohol.model.response
+
+data class AllAlcoholResponse(
+    val status: Int,
+    val message: String,
+    val data: Data
+)
+
+data class Data(
+    val alcohol: List<Alcohol>,
+    val totalCount: Int
+)
